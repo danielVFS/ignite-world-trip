4c11ec4f9ab511e4dc5dffed56754f00f3287a91
danielVFS <danielv759153@gmail.com>
2022-07-25 21:21:29 -0300

    feat(header): create component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Center, Image } from "@chakra-ui/react";
+import { PageWrapper } from "./PageWrapper";
+
+export function Header() {
+  return (
+    <PageWrapper>
+      <Center h="100" w="full">
+        <Image src="/logo.svg" alt="World trip app logo" />
+      </Center>
+    </PageWrapper>
+  );
+}
