07d18a10fc945d41cfc86f9fbab0341de62b70a7
danielVFS <danielv759153@gmail.com>
2022-07-27 16:59:12 -0300

    feat: create mock and type

---
+++ src/core/data/ContinentsData.ts
@@ -0,0 +1,34 @@
+import { Continent } from "../models/Continent";
+
+export const continents: Continent[] = [
+  {
+    name: "Europa",
+    description: "O continente mais antigo",
+    image: "/continents/Europe.png",
+  },
+  {
+    name: "América do Norte",
+    description: "O maior mercado global de viagens",
+    image: "/continents/Europe.png",
+  },
+  {
+    name: "América do Sul",
+    description: "O continente mais diversificado de natureza",
+    image: "/continents/Europe.png",
+  },
+  {
+    name: "África",
+    description: "O safari mais lindo",
+    image: "/continents/Europe.png",
+  },
+  {
+    name: "Ásia",
+    description: "Berço das principais religiões do mundo",
+    image: "/continents/Europe.png",
+  },
+  {
+    name: "Europa",
+    description: "Conheça o menor e mais novo continente",
+    image: "/continents/Europe.png",
+  },
+];
---
+++ src/core/models/Continent.ts
@@ -0,0 +1,5 @@
+export interface Continent {
+  name: string;
+  description: string;
+  image: string;
+}
