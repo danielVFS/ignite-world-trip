89cd250b24f5e48f9e525592680957a5c3f7f1bd
danielVFS <danielv759153@gmail.com>
2022-07-27 19:43:18 -0300

    feat(header): add go back function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,32 @@
-import { Center, Image } from "@chakra-ui/react";
+import { Center, Flex, Image } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { BsChevronLeft } from "react-icons/bs";
 import { PageWrapper } from "./PageWrapper";
+interface HeaderProps {
+  hasGoBackButton?: boolean;
+}
 
-export function Header() {
+export function Header({ hasGoBackButton = false }: HeaderProps) {
+  const router = useRouter();
+
+  const handleNavigateGoBack = () => {
+    router.back();
+  };
+
   return (
     <PageWrapper>
-      <Center h="100" w="full">
-        <Image src="/Logo.svg" alt="World trip app logo" />
-      </Center>
+      <Flex w="full" align="center">
+        {hasGoBackButton && (
+          <BsChevronLeft
+            color="#47585B"
+            size="25px"
+            onClick={handleNavigateGoBack}
+          />
+        )}
+        <Center h="100" w="full">
+          <Image src="/Logo.svg" alt="World trip app logo" />
+        </Center>
+      </Flex>
     </PageWrapper>
   );
 }
