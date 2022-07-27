619e8ddd9be95eb2a1aa711df1bfd071862d3c4c
danielVFS <danielv759153@gmail.com>
2022-07-27 19:47:56 -0300

    feat(continentBanner): create component

---
+++ src/components/Continent/ContinentBanner.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+
+export function ContinentBanner() {
+  return (
+    <Flex
+      w="full"
+      h="500"
+      bgImage="url('/Banner.png')"
+      bgPosition="center"
+      bgSize="cover"
+    />
+  );
+}
