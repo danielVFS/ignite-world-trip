f2d3f53c7b32979266ccf8415fd0c95c9abc57a1
danielVFS <danielv759153@gmail.com>
2022-07-27 17:09:30 -0300

    feat(swiperSlide): create continent slide

--- src/components/Home/ContinentSwiper.tsx
+++ src/components/Home/ContinentSwiper.tsx
@@ -4,9 +4,12 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { continentsData } from "../../core/data/ContinentsData";
 import { PageWrapper } from "../PageWrapper";
 
 export function ContinentSwiper() {
+  const continents = continentsData;
+
   return (
     <PageWrapper>
       <Flex w="full" mb={8} mt={12}>
@@ -16,32 +19,28 @@ export function ContinentSwiper() {
           scrollbar={{ draggable: true }}
           navigation
         >
-          <SwiperSlide>
-            <Flex
-              w="full"
-              h="450"
-              align="center"
-              justify="center"
-              direction="column"
-              bgImage="url('/Continent.png')"
-              bgRepeat="no-repeat"
-              bgSize="cover"
-              textAlign="center"
-            >
-              <Heading color="gray.100">Europa</Heading>
-              <Text color="gray.300" fontSize="lg">
-                O Continente mais antigo
-              </Text>
-            </Flex>
-          </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          {continents.map((continent) => {
+            return (
+              <SwiperSlide key={continent.description}>
+                <Flex
+                  w="full"
+                  h="450"
+                  align="center"
+                  justify="center"
+                  direction="column"
+                  bgImage={`url(${continent.image})`}
+                  bgRepeat="no-repeat"
+                  bgSize="cover"
+                  textAlign="center"
+                >
+                  <Heading color="gray.100">{continent.name}</Heading>
+                  <Text color="gray.300" fontSize="lg">
+                    {continent.description}
+                  </Text>
+                </Flex>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </Flex>
     </PageWrapper>
