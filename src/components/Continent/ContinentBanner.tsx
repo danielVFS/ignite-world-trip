e0e52cf1e5e533d625a0b5b9dccbcc4110782957
danielVFS <danielv759153@gmail.com>
2022-07-28 19:55:06 -0300

    refactor(continentBanner): update heigth

--- src/components/Continent/ContinentBanner.tsx
+++ src/components/Continent/ContinentBanner.tsx
@@ -4,7 +4,7 @@ export function ContinentBanner() {
   return (
     <Flex
       w="full"
-      h="500"
+      h={[200, 300, 500]}
       bgImage="url('/Banner.png')"
       bgPosition="center"
       bgSize="cover"
