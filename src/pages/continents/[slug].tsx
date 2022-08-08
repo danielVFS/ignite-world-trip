78f975336b3c21a416439d7075cc4dc248ba5045
danielVFS <danielv759153@gmail.com>
2022-08-08 11:09:34 -0300

    refactor(ContinentPage): using getstaticpaths

--- src/pages/continents/[slug].tsx
+++ src/pages/continents/[slug].tsx
@@ -1,5 +1,5 @@
 import { isEmpty } from "lodash";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -8,10 +8,15 @@ import { ContinentCitiesList } from "../../components/Continent/ContinentCitiesL
 import { ContinentInfo } from "../../components/Continent/ContinentInfo";
 import { Header } from "../../components/Header";
 import { Loader } from "../../components/Loader";
+import { Continent } from "../../core/models/Continent";
 import { useContinentContext } from "../../hooks/useContinentContext";
 import { useContinentInfo } from "../../hooks/useContinentInfo";
 
-const Continent: NextPage = () => {
+interface ContinentPageProps {
+  continents: Continent[];
+}
+
+const ContinentPage: NextPage<ContinentPageProps> = ({ continents }) => {
   const router = useRouter();
   const { slug } = router.query;
   const [isLoading, setIsLoading] = useState(true);
@@ -47,4 +52,24 @@ const Continent: NextPage = () => {
   );
 };
 
-export default Continent;
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: "europe" } },
+      { params: { slug: "north-america" } },
+      { params: { slug: "africa" } },
+      { params: { slug: "oceania" } },
+    ],
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<ContinentPageProps> = async () => {
+  return {
+    props: {
+      continents: [],
+    },
+  };
+};
+
+export default ContinentPage;
